09deb08cb4c0935ec62133aa5c2ad690298fe226
AlexKrylov <krylofffff93@gmail.com>
2022-10-17 20:29:32 +0300

    task 2 complete+

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // task 1
+        System.out.println("Task 1");
         var dog =8;
         System.out.println(dog);
         var cat = 3.6;
@@ -8,7 +8,7 @@ public class Main {
         var paper = 763789;
         System.out.println(paper);
 
-        // task2
+        System.out.println("Task 2");
         dog+=4;
         System.out.println(dog);
         cat+=4;
